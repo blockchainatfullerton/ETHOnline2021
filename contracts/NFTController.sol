88768bd6e84e96bf162690d68fde602a770c850f
dannytzoc <tzocdanny@gmail.com>
2021-10-10 16:31:33 -0700

    simple nft

--- contracts/NFTController.sol
+++ contracts/NFTController.sol
@@ -1,47 +1,49 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import "./Mocks/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract NFTController {
-IERC721 public nonFungibleContract;
+
 
   struct Auction {
+    IERC721 nftToAuction;
+    address NFT;
     address seller;
-    uint128 price;
+    uint endtime; 
     uint createTime;
   }
 
   mapping (uint256 => Auction) public tokenIdToAuction;
 
-  function TokenAuction( address _nftAddress ) external {
-    nonFungibleContract = IERC721(_nftAddress);
-  }
+  
 
-  function createAuction( uint256 _tokenId, uint128 _price) external {
-    nonFungibleContract.safeTransferFrom(msg.sender,address(this),_tokenId);
+  function createAuction( address _nftAddress , uint256 _tokenId,  uint _endtime) external {
+  
     Auction memory _auction = Auction({
        seller: msg.sender,
-       price: uint128(_price),
-       createTime: block.timestamp
+       createTime: block.timestamp,
+       NFT: _nftAddress,
+       endtime: _endtime,
+       nftToAuction: IERC721(_nftAddress)
     });
+     _auction.nftToAuction.safeTransferFrom(msg.sender,address(this),_tokenId);
     tokenIdToAuction[_tokenId] = _auction;
   }
 
   function bid( uint256 _tokenId ) external payable {
     Auction memory auction = tokenIdToAuction[_tokenId];
     require(auction.seller != address(0));
-    require(msg.value >= auction.price);
-    require(block.timestamp < auction.createTime);
+    require(auction.createTime < auction.endtime+auction.createTime);
 }
 
   function cancel( uint256 _tokenId ) external {
     Auction memory auction = tokenIdToAuction[_tokenId];
     require(auction.seller == msg.sender);
-
+    auction.nftToAuction.safeTransferFrom(address(this),msg.sender,_tokenId);
     delete tokenIdToAuction[_tokenId];
 
-    nonFungibleContract.safeTransferFrom(address(this),msg.sender, _tokenId);
+    
   }
 
 
