d67cea2350edd0b309f5851e215461d641f7e0f6
dannytzoc <tzocdanny@gmail.com>
2021-10-15 20:20:20 -0700

    starting test

--- test/controller-test.js
+++ test/controller-test.js
@@ -8,7 +8,8 @@ describe("Controller contract", function () {
     const BasicNFT = await ethers.getContractFactory("SimpleNFT");
 
     const BasicNFTToken = await BasicNFT.deploy("SimpleNFT","SNFT");
-
+    const NFTC = await ethers.getContractFactory("NFTController");
+    const NFTController = await NFTC.deploy()
     await BasicNFTToken.deployed();
 
     expect(await BasicNFTToken.name()).to.equal("SimpleNFT");
